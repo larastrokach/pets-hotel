ae16e692cb3f12455dc8d81ed58a03b31e300ebd
larastrokach <larastrokach@gmail.com>
2023-07-27 21:50:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+gsap.to (".navbar-brand", {delay: 1, rotation: 360, duration:2,  ease: "power2"})
+gsap.to (".picture", { delay: 4, x:700, duration:3, opacity:0})
+gsap.from (".phrase", { delay: 3, opacity:0, duration:2})
